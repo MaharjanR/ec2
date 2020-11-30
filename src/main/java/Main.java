605f34a079a4d598e333297c70dfdbdfc3123665
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-11-30 11:54:45 -0500

    Changed src

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Practising jenkins on ec2");
-        System.out.println("asdasdsadsa");
         System.out.println("Testing 2");
         System.out.println("Changed to shell instead of windows batch");
+        System.out.println("Changed the source");
     }
 }
